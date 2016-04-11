c1cc54ac8a206aa009aaf4e3f92fbf177b8aa488
feifang <fangfei2353@126.com>
2016-04-11 10:40:31 +0800

    finished most work about tweets trending, one tiny issue about retweet to fix

--- tweet_trending.py
+++ tweet_trending.py
@@ -31,17 +31,23 @@ def load_json_from_file(data):
 			tweets.append(json.loads(line))
 	return tweets
 
-#def save_list_to_csv(data, fields, file):
+def save_list_to_csv(data, fields, filename):    #fields - String
+	with open(outfile%filename,'w') as out:
+		# use print instead of write to avoid UnicodeEncodeError
+		print(fields, file = out)
+		for row in data:
+				print(str(row)[1:-1], file = out)    # get rid of "(" and ")"
 	
 
-# print table facility
+# print table facility for command line
 # e.g. print_cmd_table(get_top_retweets(tweets)[:10], ['Count', 'Screen Name', 'Text'], 'Text')
-def print_cmd_table(data, fields, max_width_col):
+def print_cmd_table(data, fields, max_width_col):   #fields - List
 	pt = PrettyTable(field_names = fields)
-	[ pt.add_row(row) for row in data[:10]]
+	[ pt.add_row(row) for row in data[:10]]   # top 10
 	pt.max_width[max_width_col] = 50
 	pt.align = 'l'
 	print (pt)
+
 
 def get_top_retweets(tweets, save_to_csv = True):
 	# Bug To Fix: ids are not unique, should only leave the tweets with largest count if duplicated. 
@@ -58,36 +64,56 @@ def get_top_retweets(tweets, save_to_csv = True):
 					if tweet.has_key('retweeted_status')
 				], key = getKey, reverse = True)
 	if save_to_csv:
-		with open(outfile%'top_retweets.csv','w') as out:
-			# use print instead of write to avoid UnicodeEncodeError
-			print("retweet_count,tweet_id,screen_name,text", file = out)
-			for row in retweets:
-				print(str(row)[1:-1], file = out)    # get rid of "(" and ")"
+		header = "retweet_count,tweet_id,screen_name,text"    
+		save_list_to_csv(retweets, header, "top_retweets.csv")
 	return retweets
 	
-def get_top_entities(tweets, save_to_csv = True):
+def get_top_entities(tweets, save_to_csv = True, print_to_cmd = True):
 	screen_names = []
+	hashtags = []
+	urls = []
+	symbols = []
+	media = []
 	for tweet in tweets:
 		entities = tweet['entities']
 		if entities['screen_names']: screen_names += entities['screen_names']
-	c = Counter(screen_names)
-	print (c.most_common()[:10])
+		if entities['hashtags']: hashtags += entities['hashtags']
+		if entities['urls']: urls += entities['urls']
+		if entities['symbols']: symbols += entities['symbols']
+		if entities['media']: media += entities['media']
+	top_entities = {
+		'top_screen_names': Counter(screen_names).most_common(),
+		'top_hashtags': Counter(hashtags).most_common(),
+		'top_urls': Counter(urls).most_common(),
+		'top_symbols': Counter(symbols).most_common(),
+		'top_media': Counter(media).most_common()
+	}
+	for key, value in top_entities.iteritems():
+		if save_to_csv:    # save Top 50
+			save_list_to_csv(value[:50], "%s,count"%key, "%s.csv"%key)
+		if print_to_cmd:    # print Top 10
+			print_cmd_table(value, [key, "count"], key)
+	return top_entities
 
-def get_trends(tweets):
-	trends = {}
-	trends['tweets_count'] = len(tweets)
-	trends['users_count'] = count_unique_users(tweets)
-	trends['top_retweets'] = get_top_retweets(tweets)[:50]
-	print (trends)
-	
-
-			
+def get_trends(data, filename = None):
+	tweets = load_json_from_file(data)
+	tweets_count = len(tweets)
+	users_count = count_unique_users(tweets)
+	top_retweets = get_top_retweets(tweets)[:50]
+	trends = {
+		'tweets_count': tweets_count,
+		'users_count': users_count,
+		'top_retweets': top_retweets,
+		'top_entities': get_top_entities(tweets)
+	}
+	if filename:
+		with open(outfile% filename, 'w') as out:
+			out.write(json.dumps(trends, indent=4))
+		
 
 if __name__ == '__main__':
-	tweets = load_json_from_file(data)
-	#get_trends(tweets)
-	#get_top_entities(tweets)
-	get_top_retweets(tweets)
+	get_trends(data, "trends.json")
+	
 	
 	
 	
